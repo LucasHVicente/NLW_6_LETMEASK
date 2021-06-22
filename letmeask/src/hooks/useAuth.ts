171f4968abee2213b3cf5ec5a1870db72da1cda1
Lucas Vicente <lucas.vicente@nappsolutions.com>
2021-06-22 17:36:41 -0300

    aula-2 hook

--- letmeask/src/hooks/useAuth.ts
+++ letmeask/src/hooks/useAuth.ts
@@ -1,1 +1,4 @@
-export const useAuth = useContext(AuthContext)
+import { useContext } from "react";
+import { AuthContext } from "../contexts/auth";
+
+export function useAuth() {return useContext(AuthContext)}
